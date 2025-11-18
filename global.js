94f12b7800494233674bd795a67181995872d6d4
ShortStop-info <info@shortstop.co.jp>
2025-11-18 18:14:10 +0900

    Implement click toggle for QA items
    Add event listener for question click to toggle active class.

--- global.js
+++ global.js
@@ -1,1 +1,11 @@
-// There is currently no JavaScript code here.
+// There is currently no JavaScript code here.// There is currently no JavaScript code here.
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".qa-item").forEach(item => {
+        const question = item.querySelector(".qa-question");
+
+        question.addEventListener("click", () => {
+            item.classList.toggle("active");
+        });
+    });
+});
